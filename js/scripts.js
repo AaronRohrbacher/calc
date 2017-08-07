7d1449510e9fe3d09915f94323648b72c3386a11
Aaron Rohrbacher and Noah Gottlieb <aaron@>
2017-08-07 11:04:10 -0700

    clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,11 @@ return number1 / number2;
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number, you stupid!"));
 alert(divide(number1,number2));
+
+var add = function (number1, number2) {
+return number1 + number2;
+
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number, you stupid!"));
