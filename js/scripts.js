acb486f26c1902b3c909b54aae777330d26506e5
Aaron Rohrbacher and Noah Gottlieb <noah@>
2017-08-07 10:07:39 -0700

    create html and js files for calc

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function (number1, number2) {
+return parseFloat(number1) + parseFloat(number2);
+
+};
+
+var number1 = prompt("Enter a number:");
+var number2 = prompt("Enter another number, you stupid!")
+alert(add(number1,number2));
