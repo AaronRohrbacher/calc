246d9f1acaebbea92426d964c4337314ba94e9bf
Aaron Rohrbacher and Noah Gottlieb <noah@>
2017-08-07 10:17:33 -0700

    add subtraction, multiplication, and division functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,35 @@
 var add = function (number1, number2) {
-return parseFloat(number1) + parseFloat(number2);
+return number1 + number2;
 
 };
 
-var number1 = prompt("Enter a number:");
-var number2 = prompt("Enter another number, you stupid!")
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number, you stupid!"));
 alert(add(number1,number2));
+
+var sub = function (number1, number2) {
+return number1 - number2;
+
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number, you stupid!"));
+alert(sub(number1,number2));
+
+var mult = function (number1, number2) {
+return number1 * number2;
+
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number, you stupid!"));
+alert(mult(number1,number2));
+
+var divide = function (number1, number2) {
+return number1 / number2;
+
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number, you stupid!"));
+alert(divide(number1,number2));
